a3e1cf33f0fafee20879c5a51849c7115f6968fc
Christian Prochnow <christian.prochnow77@gmail.com>
2019-04-26 13:38:17 +0200

    Added validator registration.

--- src/AppCore.Validation.FluentValidation/FluentValidationRegistrationExtensions.cs
+++ src/AppCore.Validation.FluentValidation/FluentValidationRegistrationExtensions.cs
@@ -28,17 +28,38 @@ namespace AppCore.DependencyInjection
             return builder.AddExtension<FluentValidationExtension>();
         }
 
-        public static IFacilityExtensionBuilder<IValidationFacility, FluentValidationExtension> UseValidators(
+        /// <summary>
+        /// Registers <see cref="FluentValidation.IValidator{T}"/> components.
+        /// </summary>
+        /// <param name="builder">The <see cref="IFacilityExtensionBuilder{TFacility, TExtension}"/>.</param>
+        /// <param name="register">The validator registration action.</param>
+        /// <returns>The <see cref="IFacilityExtensionBuilder{TFacility, TExtension}"/>.</returns>
+        public static IFacilityExtensionBuilder<IValidationFacility, FluentValidationExtension> AddValidators(
             this IFacilityExtensionBuilder<IValidationFacility, FluentValidationExtension> builder,
             Action<IRegistrationBuilder, IValidationFacility> register)
         {
             Ensure.Arg.NotNull(builder, nameof(builder));
+            Ensure.Arg.NotNull(register, nameof(register));
+
             builder.AddExtension(
                 new RegistrationFacilityExtension<IValidationFacility>(
                     typeof(FluentValidation.IValidator<>),
                     register));
 
             return builder;
+        }
+
+        /// <summary>
+        /// Registers <see cref="FluentValidation.IValidator{T}"/> components.
+        /// </summary>
+        /// <param name="builder">The <see cref="IFacilityExtensionBuilder{TFacility, TExtension}"/>.</param>
+        /// <param name="register">The validator registration action.</param>
+        /// <returns>The <see cref="IFacilityExtensionBuilder{TFacility, TExtension}"/>.</returns>
+        public static IFacilityExtensionBuilder<IValidationFacility, FluentValidationExtension> AddValidators(
+            this IFacilityExtensionBuilder<IValidationFacility, FluentValidationExtension> builder,
+            Action<IRegistrationBuilder> register)
+        {
+            return builder.AddValidators((r, _) => register(r));
         }
     }
 }
